0511789edc9520cad17a6d322c233259877b5b06
ZSX-JOJO <LOLZSXZXM@163.COM>
2017-06-23 20:55:08 +0800

    卧槽 阔以的

---
+++ timeAgo/demo/index.js
@@ -0,0 +1,17 @@
+function iso8601(date) {
+  return date.getUTCFullYear()
+    + "-" + (date.getUTCMonth()+1)
+    + "-" + date.getUTCDate()
+    + "T" + date.getUTCHours()
+    + ":" + date.getUTCMinutes()
+    + ":" + date.getUTCSeconds() + "Z";
+}
+
+document.querySelector('.load_time').setAttribute('datetime', iso8601(new Date()));
+var timeagoInstance = timeago(null, navigator.language.replace('-', '_'));
+timeagoInstance.render(document.querySelectorAll('.need_to_be_rendered'));
+
+// 2. demo
+document.getElementById('demo_now').innerHTML = timeago().format(new Date());
+document.getElementById('demo_20160907').innerHTML = timeago(null, 'zh_CN').format('2016-09-07');
+document.getElementById('demo_timestamp').innerHTML = timeago().format(1473245023718); 
