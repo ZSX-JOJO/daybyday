ed6dae168b2349aabec14c91659a7c7dbe3f86a4
ZSX-JOJO <LOLZSXZXM@163.COM>
2018-05-09 17:55:02 +0800

    不再乱说话.....

---
+++ Node-begin/hello.js
@@ -0,0 +1,21 @@
+'use strict';//严格模式
+/*
+命令行模式和 Node 交互模式
+ */
+/*
+理解 AMD ，CMD，CommonJS 规范
+ */
+/**
+ * process也是 Node.js 提供的一个对象，它代表当前 Node.js 进程
+ * 
+ */
+/**
+ * Javascript 语言将任务的执行模式分成两种：同步（Synchronous）和异步（Asynchronous）
+ * "异步模式" 编程的 4 种方法
+ * 	一、回调函数
+ * 	 f1();
+　　 f2();
+    f1(f2);
+    二、事件监听
+ */
+console.log('Hello, world.');
