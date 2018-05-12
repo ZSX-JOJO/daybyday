63de393654cad5b6ae1387dab701b733bbdd2872
ZSX-JOJO <LOLZSXZXM@163.COM>
2018-05-12 17:41:43 +0800

    测试

--- Node/app.js
+++ Node/app.js
@@ -1,7 +1,17 @@
-setTimeout(() =>{
-	console.log("箭头函数");
-},3000);
+// setTimeout(() =>{
+// 	console.log("箭头函数");
+// },3000);
 
-setTimeout(function(){
-	console.log("传统函数");
-},3000);
+// setTimeout(function(){
+// 	console.log("传统函数");
+// },3000);
+
+/*
+事件
+*/
+var events = require('events');
+var myEmitter = new events.EventEmitter();
+myEmitter.on('someEvent',function(message){
+	console.log(message);
+});
+myEmitter.emit('someEvent','the envent was emitted');
