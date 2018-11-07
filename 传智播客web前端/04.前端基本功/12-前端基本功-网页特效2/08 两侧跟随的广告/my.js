2ec3494861552b256eb894f4ffa6849a061f54ac
ZSX-JOJO <LOLZSXZXM@163.COM>
2018-11-07 17:55:26 +0800

    去他娘的吧

---
+++ 传智播客web前端/04.前端基本功/12-前端基本功-网页特效2/08 两侧跟随的广告/my.js
@@ -0,0 +1,25 @@
+/**
+ * Created by andy on 2015/11/19.
+ */
+function $(id) {return document.getElementById(id);}
+function scroll() {
+    if(window.pageYOffset != null)  //  ie9+ 和其他浏览器
+    {
+        return {
+            left: window.pageXOffset,
+            top: window.pageYOffset
+        }
+    }
+    else if(document.compatMode == "CSS1Compat")  // 声明的了 DTD
+    // 检测是不是怪异模式的浏览器 -- 就是没有 声明<!DOCTYPE html>
+    {
+        return {
+            left: document.documentElement.scrollLeft,
+            top: document.documentElement.scrollTop
+        }
+    }
+    return { //  剩下的肯定是怪异模式的
+        left: document.body.scrollLeft,
+        top: document.body.scrollTop
+    }
+}
