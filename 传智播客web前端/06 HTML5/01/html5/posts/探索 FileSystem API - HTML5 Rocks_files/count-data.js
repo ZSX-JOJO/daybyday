e74f864ef977d030b452a540b50c43f1db3dc71c
ZSX-JOJO <LOLZSXZXM@163.COM>
2018-11-13 18:26:17 +0800

    23333

---
+++ 传智播客web前端/06 HTML5/01/html5/posts/探索 FileSystem API - HTML5 Rocks_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0","multiple":"{num}","one":"1"}},"counts":[{"id":"http:\/\/www.html5rocks.com\/tutorials\/file\/filesystem\/","comments":152}]});
+}
