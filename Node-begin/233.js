36fbf7a353d96a9fdeab81ec03311776c3f33cae
ZSX-JOJO <LOLZSXZXM@163.COM>
2019-03-28 21:10:58 +0800

    删除不相干

--- Node-begin/233.js
+++
@@ -1,3 +0,0 @@
-setTimeout(function(){
-  console.log("233333");
-},4000);
--- Node-begin/json-server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-function json(req,res){
-	console.log("INCOMING REQUEST:" +req.method+""+req.url);
-	res.writeHead(200,{"Content-Type":"application/json"});
-	res.end(JSON.stringify({err:null})+"\n");
-}
-var s = http.createServer(json);
-s.listen(8080);
